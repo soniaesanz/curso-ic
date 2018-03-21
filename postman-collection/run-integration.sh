ec67538bbe3bbb64d2271edc6a61dd49e3e14de5
Matias <matias.gonzalez@grupoesfera.com.ar>
2018-03-21 14:36:35 -0300

    WIP

--- postman-collection/run-integration.sh
+++ postman-collection/run-integration.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sleep 60s
+sleep 2m
 
 docker run --rm -v $WORKSPACE/postman-collection:/etc/newman  \
                     postman/newman_ubuntu1404  run "demo-api.json.postman_collection"  \
