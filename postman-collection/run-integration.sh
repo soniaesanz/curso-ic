4673824863720e2f601fd97a1075665cf4033bc6
Matias <matias.gonzalez@grupoesfera.com.ar>
2018-03-20 23:39:00 -0300

    WIP Jenkinsfile

---
+++ postman-collection/run-integration.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+docker run -v /var/lib/docker/volumes/jenkins-data/_data/workspace/demo-3_develop-PCXMIVKOQCVF5ZGT4TOCQXASPPPMZQZWMEO2JUUU4OGUCK6ZVN4Q/postman-collection:/etc/newman  \
+postman/newman_ubuntu1404  run "demo-api.json.postman_collection"  \
+--disable-unicode  --environment="test.json.postman_environment" \
+--reporters="junit,cli"
