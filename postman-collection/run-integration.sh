316782b883c0c742d3f36c8c97daa34a75d2fba6
Matias <matias.gonzalez@grupoesfera.com.ar>
2018-03-21 16:02:57 -0300

    WIP

--- postman-collection/run-integration.sh
+++ postman-collection/run-integration.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-
-sleep 2m
-
 docker run --rm -v $WORKSPACE/postman-collection:/etc/newman  \
                     postman/newman_ubuntu1404  run "demo-api.json.postman_collection"  \
                     --disable-unicode  --environment="test.json.postman_environment" \
