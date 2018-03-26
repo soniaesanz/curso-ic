5008f6e36c4773d02d03382adb146be0ba1d9b33
Matias <matias.gonzalez@grupoesfera.com.ar>
2018-03-26 10:45:28 -0300

    refactor

--- integration-test/run-integration.sh
+++ integration-test/run-integration.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker run --rm -v $WORKSPACE/postman-collection:/etc/newman  \
+docker run --rm -v $WORKSPACE/integration-test:/etc/newman  \
                     postman/newman_ubuntu1404  run "demo-api.json.postman_collection"  \
                     --disable-unicode  --environment="test.json.postman_environment" \
                     --reporters="junit,cli"
